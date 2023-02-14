03b4f7371bacb97b9bb97ef5baaf08e211e1f0d4
Edgar Caudron <edgar.caudron@gmail.com>
2023-02-14 19:32:01 +0100

    Website update

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -6,6 +6,7 @@ import Footer from '@/components/Footer'
 import { Drawer, Sidebar } from '@/components/Sidebar'
 import '@/styles/globals.css'
 import { useRouter } from 'next/router'
+import Script from 'next/script';
 
 const MyApp = ({ Component, pageProps }) => {
   	const router = useRouter()
@@ -14,32 +15,49 @@ const MyApp = ({ Component, pageProps }) => {
     });
 
 	return (
-		<div className="drawer drawer-mobile">
-        	<input id="main-menu" type="checkbox" className="drawer-toggle" />
-        	<div className="drawer-content flex flex-col content-around items-center">
-				<Navbar />
+		<>
+			<Script
+		        src="https://www.googletagmanager.com/gtag/js?id=G-73P1TDHSHM"
+		        strategy="afterInteractive"
+		      />
+			<Script id="google-analytics" strategy="afterInteractive">
+			{`
+				window.dataLayer = window.dataLayer || [];
+				function gtag(){dataLayer.push(arguments);}
+				gtag('js', new Date());
 
-                {router.route.split('/')[2] === "admin" ? (
-                	<div className="flex">
-                		<div className="hidden md:flex flex-none w-56">
-                			<Sidebar />
-                		</div>
+				gtag('config', 'G-73P1TDHSHM');
+			`}
+			</Script>
 
-                		<div className="max-w-7xl grow">
-                			<Component {...pageProps} />
-                		</div>
-                	</div>
-                ) : (
-                	<div className="max-w-7xl grow">
-            			<Component {...pageProps} />
-            		</div>
-                )}
+			<div className="drawer drawer-mobile">
+	        	<input id="main-menu" type="checkbox" className="drawer-toggle" />
+	        	<div className="drawer-content flex flex-col content-around items-center">
+					<Navbar />
 
-				<Footer />
+	                {router.route.split('/')[2] === "admin" ? (
+	                	<div className="w-full flex grow justify-between">
+	                		<div className="hidden md:flex flex-none w-56">
+	                			<Sidebar />
+	                		</div>
+
+	                		<div className="max-w-7xl grow flex flex-col justify-between">
+	                			<Component {...pageProps} />
+								<Footer />
+	                		</div>
+	                	</div>
+	                ) : (
+	                	<div className="w-full max-w-7xl grow flex flex-col justify-between">
+	            			<Component {...pageProps} />
+							<Footer />
+	            		</div>
+	                )}
+
+				</div>
+
+				<Drawer />
 			</div>
-
-			<Drawer />
-		</div>
+		</>
 	)
 }
 
